fd09b97f5ef8b87ec3ee4440fcaa4cd971ce3f92
Herkus Savickas <herkus.savickas@gmail.com>
2026-06-16 16:34:09 +0200

    Added comments to the scripts that I added

--- MedicineManager.cs
+++ MedicineManager.cs
@@ -4,6 +4,11 @@ using System.Collections.Generic;
 
 static class MedicineManager
 {
+    //The medicine manager is a static class responsible for defining
+    //all the different types of medicine available.
+    //Imagine this script as a database for all the available types of medicine.
+    //This can also easily be expanded, it is a modular system.
+    //In order to access individual members, type MedicineManager.Database["<Insert medicine name here>"]
     public static Dictionary<string, Medicine> Database = new()
     {
         ["Morphine"] = new Medicine { name = "Morphine", cost = 11 },
