5c917023e9482a4bd46cd1c8ffad2ed4578122e7
Herkus Savickas <herkus.savickas@gmail.com>
2026-06-19 16:44:17 +0200

    Hallway implementation, part 2
    Added a door class which takes you to a patient room. There should also now be multiple patient rooms, not just 1.

--- Hallway.cs
+++ Hallway.cs
@@ -7,10 +7,13 @@ public partial class Hallway : Node2D
 	Control HallwayControl;
 	Control DoorControl;
     List<Button> Doors =  new List<Button>();
+    Main MainNode;
 
     public override void _Ready()
     {
-		HallwayInitialize();
+        MainNode = (Main)GetParent();
+        //CloseFundsPopup.Pressed += () => CloseParent(CloseFundsPopup);
+       // HallwayInitialize();
     }
     public void HallwayInitialize()
 	{
@@ -18,23 +21,28 @@ public partial class Hallway : Node2D
         DoorControl = HallwayControl.GetNode<Control>("DoorControl");
 
         //Doors
-
+        int doorIndex = 0;
         foreach (Node child in DoorControl.GetChildren())
         {
             if (child.GetClass() == "Button")
             {
                 Button childButton = child as Button;
                 Doors.Add(childButton);
-                childButton.Pressed += GoToRoom;
+                Door doorButton = childButton as Door;
+                doorButton.doorId = doorIndex;
+                doorIndex++;
+                childButton.Pressed += () => GoToRoom(doorButton.doorId);
             }
         }
     }
 
-    private void GoToRoom()
+    private void GoToRoom(int index)
     {
         Hide();
 
-        var RoomScene = (Node2D)GetParent().GetNode("Room");
+        //var RoomScene = (Node2D)GetParent().GetNode("Room");
+        //GD.Print($"Room count: {RoomList.Count}.");
+        var RoomScene = MainNode.RoomList[index];
         RoomScene.Show();
 
         //push the scene we're entering to the previous scenes stack
--- Main.cs
+++ Main.cs
@@ -1,14 +1,33 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public partial class Main : Node
 {
-	// Called when the node enters the scene tree for the first time.
+    // Called when the node enters the scene tree for the first time.
+    [Export] Control RoomControl;
+    public List<Node2D> RoomList = new List<Node2D>();
+    int finalRoomCount = 6;
 	public override void _Ready()
 	{
         //always keep the office at the bottom of the previous scenes stack, so the reference on how to return to it is always there
         GlobalData.PreviousScenes.Push(GetNode("Office").GetPath());
+        GeneratePatientRooms(RoomControl);
+        Hallway hallway = GetNode<Node2D>("Hallway") as Hallway;
+        hallway.HallwayInitialize();
 	}
+
+    private void GeneratePatientRooms(Control roomControl)
+    {
+        var patientRoom = (Node2D)GetNode("Room");
+        for (int i = 0; i < finalRoomCount; i++)
+        {
+            Node2D newRoom = (Node2D)patientRoom.Duplicate();
+            newRoom.Hide();
+            roomControl.AddChild(newRoom);
+            RoomList.Add(newRoom);
+        }
+    }
 
     public override void _UnhandledInput(InputEvent @event)
     {
@@ -28,13 +47,7 @@ public partial class Main : Node
                 var parent = (Node2D)GetNode(GlobalData.PreviousScenes.Peek().ToString());
                 //show it
                 parent.Show();
-
             }
         }
     }
-
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(double delta)
-	{
-	}
 }
